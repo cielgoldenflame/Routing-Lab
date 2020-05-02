ec49c134797ac40f93c503f7f425e340844fb3df
cielgoldenflame <chris.lichner@outlook.com>
2020-05-02 14:32:18 -0400

    creation of VMs and networks successful

--- Routing-Lab-Setup.ps1
+++ Routing-Lab-Setup.ps1
@@ -1,4 +1,5 @@
 $rgname = Read-Host -Prompt "What would you like to be your Resource Group name?`n"
+Clear-Host
 $x = Read-Host -Prompt "Where would you like your Resource Group located?
 
 1 : Central US
@@ -20,61 +21,82 @@ switch ($x) {
 
 New-AzResourceGroup -Name $rgname -Location $rglocation
 
+Clear-Host
+
 $vnetname = Read-Host -Prompt "What will be the name of your VNet?`n"
 
 New-AzVirtualNetwork -AddressPrefix '10.0.0.0/16' -Name $vnetname -Location $rglocation -ResourceGroupName $rgname
 
 $vnet = Get-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rgname
 
+Clear-Host
+
 [int]$n = Read-Host -Prompt "How many subnets will there be?`n"
 $i = 0
 while ($i -lt $n) {
+    Clear-Host
+
     $subname = Read-Host -Prompt "What will be the name of Subnet $i`n"
     Add-AzVirtualNetworkSubnetConfig -Name $subname -VirtualNetwork $vnet -AddressPrefix "10.0.$i.0/24" | Set-AzVirtualNetwork
     ++$i
 }
 
+Clear-Host
+
+$asname = Read-Host -Prompt "What will your availability set be called?`n"
+
+New-AzAvailabilitySet -ResourceGroupName $rgname -Name $asname -Location $rglocation -PlatformUpdateDomainCount "3" -PlatformFaultDomainCount "2" -Sku Aligned
+
+Clear-Host
+
 [int]$vmx = Read-Host -Prompt "How many VMs will you need?`n" 
 
 $vnet = Get-AzVirtualNetwork -Name $vnetname -ResourceGroupName $rgname
 
+$o = 0
 while ($o -lt $vmx) {
-    
+
+    Clear-Host
+
     $vmName = Read-Host -Prompt "What will be the name of VM $o`n"
-    
+
+    Clear-Host
+
     $version = Read-Host -Prompt "Will VM $o be:`n1 : Server`n2 : Client`n"
 
     switch ($version) {
-        '1' { $publisherName = 'MicrosoftWindowsServer'
-            $offerName = 'WindowsServer'
-            $skuName = '2016-Datacenter'}
-        '2' { $publisherName = 'microsoftwindowsdesktop'
-            $offerName = 'windows-10-1809-vhd-client-office-prod-stage'
-            $skuName = 'rs5-evd-o365pp'}
+        '1' {   $publisherName = 'MicrosoftWindowsServer'
+                $offerName = 'WindowsServer'
+                $skuName = '2016-Datacenter'}
+        '2' {   $publisherName = 'MicrosoftWindowsDesktop'
+                $offerName = 'Windows-10'
+                $skuName = 'rs5-pro'}
     }
     
-    $size = Read-Host -Prompt "Please select VM Size`n1 : Standard_D1_v2 (1 CPU 3.5GB RAM)`n2 : Standard_D2_v2 (2 CPU 7GB RAM)`n"
+    Clear-Host
+
+    $size = Read-Host -Prompt "Please select VM Size`n1 : Standard_DS1_v2 (1 CPU 3.5GB RAM)`n2 : Standard_DS2_v2 (2 CPU 7GB RAM)`n"
     
     switch ($size) {
-        '1' {$vmSize = 'Standard_D1_v2'}
-        '2' {$vmSize = 'Standard_D2_v2'}
+        '1' {$vmSize = 'Standard_DS1_v2'}
+        '2' {$vmSize = 'Standard_DS2_v2'}
     }
     
     $adminUsername = 'Student'
     $adminPassword = 'Pa55w.rd1234'
     $adminCreds = New-Object PSCredential $adminUsername, ($adminPassword | ConvertTo-SecureString -AsPlainText -Force)
 
+    Clear-Host
+
     $as = Read-Host -Prompt "Will this VM be a part of a Availability Set?`nY/N"
 
     switch ($as) {
         'Y' {
-            $asname = Read-Host -Prompt "What will your availability set be called?`n"
-            
-            New-AzAvailabilitySet -ResourceGroupName $rgname -Name $asname -Location $rglocation -PlatformUpdateDomainCount "3" -PlatformFaultDomainCount "2"
-            
             $availabilitySet = Get-AzAvailabilitySet -ResourceGroupName $rgname -Name $asname 
 
             $subs = $vnet.Subnets.Name
+
+            Clear-Host
 
             $subnet = Read-Host "What subnet will this VM be attached to?`n$subs`n"
             
@@ -89,13 +111,15 @@ while ($o -lt $vmx) {
             Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
             Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $adminCreds
             Set-AzVMSourceImage -VM $vmConfig -PublisherName $publisherName -Offer $offerName -Skus $skuName -Version 'latest'
-            Set-AzVMOSDisk -VM $vmConfig -Name "$($vmName)_OsDisk_1_$(Get-Random)"  -CreateOption fromImage
+            Set-AzVMOSDisk -VM $vmConfig -Name "$($vmName)_OsDisk_1_$(Get-Random)" -StorageAccountType Premium_LRS -CreateOption fromImage
             Set-AzVMBootDiagnostic -VM $vmConfig -Disable
 
-            New-AzVM -ResourceGroupName $rgname -Location $rglocation -VM $vmConfig
+            New-AzVM -ResourceGroupName $rgname -Location $rglocation -VM $vmConfig -AsJob
         }
         'N' {
             $subs = $vnet.Subnets.Name
+
+            Clear-Host
 
             $subnet = Read-Host "What subnet will this VM be attached to?`n$subs`n"
             
@@ -110,10 +134,10 @@ while ($o -lt $vmx) {
             Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
             Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $adminCreds
             Set-AzVMSourceImage -VM $vmConfig -PublisherName $publisherName -Offer $offerName -Skus $skuName -Version 'latest'
-            Set-AzVMOSDisk -VM $vmConfig -Name "$($vmName)_OsDisk_1_$(Get-Random)"  -CreateOption fromImage
+            Set-AzVMOSDisk -VM $vmConfig -Name "$($vmName)_OsDisk_1_$(Get-Random)" -StorageAccountType Premium_LRS -CreateOption fromImage
             Set-AzVMBootDiagnostic -VM $vmConfig -Disable
 
-            New-AzVM -ResourceGroupName $rgname -Location $rglocation -VM $vmConfig}
+            New-AzVM -ResourceGroupName $rgname -Location $rglocation -VM $vmConfig -AsJob} 
     }
 
 ++$o
